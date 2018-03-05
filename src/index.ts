c6127e7fa23f49e5b541db58a79fe2fd8166cfe2
Chris LaRose <cjlarose@gmail.com>
2018-03-04 19:21:04 -0800

    Add rpn parser

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,43 @@ export function hello(word:  string = world): string {
 }
 
 console.log(hello());
+
+function tokenize(input: string): string[] {
+  return input.split(/\s+/);
+}
+
+type Operator = '+' | '-';
+interface ArithmeticExpression { operator: Operator; lhs: Expression; rhs: Expression; }
+type Expression = Number | ArithmeticExpression;
+
+interface ParseResult { rest: string[]; expression: Expression; }
+
+function parse(tokens: string[]): ParseResult {
+  const topToken = tokens[0];
+  if (topToken === '+') {
+    const lhs = parse(tokens.slice(1));
+    const rhs = parse(lhs.rest);
+    const res = { operator: <Operator> '+', lhs: lhs.expression, rhs: rhs.expression };
+    return { rest: rhs.rest, expression: res };
+  }
+
+  // number
+  return { rest: tokens.slice(1), expression: parseInt(tokens[0], 10) };
+}
+
+const form = document.querySelector('.input-form');
+const inputEl = <HTMLInputElement> document.querySelector('.input');
+// inputEl.focus();
+
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const text = inputEl.value;
+    console.log(text);
+    inputEl.value = '';
+
+    const tokens = tokenize(text);
+    const ast = parse(tokens);
+    console.log(ast);
+  });
+}
